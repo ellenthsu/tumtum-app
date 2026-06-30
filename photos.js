c71c131a3e8f8ab4311a48fb5f5f9fa4f776551e
Ellen Hsu <ellenhsu@ellens-air.lan>
2026-06-30 09:55:26 -0700

    Add 9 more Tum Tum photos

--- photos.js
+++ photos.js
@@ -6,4 +6,13 @@ const PHOTOS = [
   "photos/tumtum4.jpg",
   "photos/tumtum5.jpg",
   "photos/tumtum6.jpg",
+  "photos/tumtum7.jpg",
+  "photos/tumtum8.jpg",
+  "photos/tumtum9.jpg",
+  "photos/tumtum10.jpg",
+  "photos/tumtum11.jpg",
+  "photos/tumtum12.jpg",
+  "photos/tumtum13.jpg",
+  "photos/tumtum14.jpg",
+  "photos/tumtum15.jpg",
 ];
